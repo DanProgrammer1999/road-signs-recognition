f174f5d2c978b1b2d8772072f3d53d9d8ddbcbcf
Daniil Dvoryanov <daniil.dvoryanov@gmail.com>
2019-10-22 23:28:03 +0300

    Test class

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import csv
 import os
+import time
 
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score
 from sklearn.utils import shuffle
+import seaborn as sn
+import pandas as pd
 
 
 class Parameters:
@@ -15,21 +18,24 @@ class Parameters:
     test_set_annotation_filename = 'GT-final_test.csv'
 
     n_trees = 512
-    max_depth = 1000
+    max_depth = 500
     min_samples_leaf = 5
+    criterion = "gini"
 
     resize_dim = (30, 30)
-    track_length = 30
+    enable_augment = True
+
+    # Number of images that will be blended together to generate a new image for augmentation
+    augment_images_count = 10
 
     brightness_prob = 0.5
-    blur_prob = 0
 
     brightness_alpha_range = (0.5, 2)
     brightness_beta_range = (-10, 10)
 
-    blur_kernel_range = (1, 3)
-    # Number of images that will be blended together to generate a new image for augmentation
-    augment_images_count = 10
+    # Modify only if you change the dataset
+    track_length = 30
+    class_count = 43
 
 
 class DataUtils:
@@ -60,7 +66,7 @@ class DataUtils:
         validation_data, validation_labels = [], []
         start_index = 0
 
-        for c in range(43):
+        for c in range(Parameters.class_count):
             class_count = self.training_labels.count(c)
             end_index = start_index + class_count
 
@@ -88,10 +94,10 @@ class DataUtils:
         self.validation_labels = validation_labels
 
     def augment(self):
-        classes = range(43)
+        classes = range(Parameters.class_count)
         frequencies = [self.training_labels.count(c) for c in classes]
         target = max(frequencies)
-        images = [[] for _ in range(43)]
+        images = [[] for _ in range(Parameters.class_count)]
 
         for i, label in enumerate(self.training_labels):
             images[label].append(self.training_data[i])
@@ -99,7 +105,7 @@ class DataUtils:
         for i, im_class in enumerate(images):
             generate_count = target - len(im_class)
             self.training_data += DataUtils.generate_images(im_class, generate_count)
-            self.training_labels += [i]*generate_count
+            self.training_labels += [i] * generate_count
 
         return True
 
@@ -120,7 +126,7 @@ class DataUtils:
         images = []  # images
         labels = []  # corresponding labels
         # loop over all 43 classes
-        for c in range(0, 43):
+        for c in range(0, Parameters.class_count):
             prefix = os.path.join(Parameters.train_set_path, format(c, '05d'))  # subdirectory for class
             csv_filename = 'GT-' + format(c, '05d') + '.csv'
 
@@ -191,7 +197,7 @@ class DataUtils:
     @staticmethod
     def normalize_set(dataset):
         # Assuming shape is the same for all images
-        shape = len(dataset), dataset[0].shape[0]*dataset[0].shape[1]*dataset[0].shape[2]
+        shape = len(dataset), dataset[0].shape[0] * dataset[0].shape[1] * dataset[0].shape[2]
         res = np.zeros(shape, np.float)
         for i, img in enumerate(dataset):
             res[i] = DataUtils.normalize(img)
@@ -201,25 +207,18 @@ class DataUtils:
     @staticmethod
     def normalize(image):
         # res = color.rgb2gray(image)
-        res = image/np.max(image)
+        res = image / np.max(image)
         return res.flatten()
 
     @staticmethod
-    def generate_image(images, method='random'):
+    def generate_image(images):
         """
         Generate an image out of given ones and change it randomly.
         Used in augmentation
 
         :param images: images to blend together to form a new image
-        :param method: how to change the blended image. One of the options:
-                      'brightness', 'blur', or 'random'
-                      'brightness' mode transforms the image by alpha*x + beta,
-                      where alpha and beta are chosen randomly from uniform distribution
-                      'blur' applies blur kernel of random size to the picture
-                      'random' applies a combination of previous modes (or none of them)
-                      depending on the outcome of random variables
 
-        :return: an image made by blending input images and applying random transformation to them
+        :return: an image made by blending input images and applying random transformations to them
         """
 
         res_image = images[0]
@@ -227,30 +226,15 @@ class DataUtils:
             res_image = cv2.addWeighted(res_image, 0.5, images[i], 0.5, 0)
 
         # Methods: change_brightness (brightness), add_blur (blur)
-        if method == 'random':
-            probs = np.random.random(2)
-            change_brightness = probs[0] < Parameters.brightness_prob
-            add_blur = probs[1] < Parameters.blur_prob
+        prob = np.random.random(2)
+        change_brightness = prob[0] < Parameters.brightness_prob
 
-            if change_brightness:
-                res_image = DataUtils.generate_image([res_image], 'brightness')
-
-            if add_blur:
-                res_image = DataUtils.generate_image([res_image], 'blur')
-
-            return res_image
-
-        elif method == 'brightness':
+        if change_brightness:
             alpha = np.random.uniform(*Parameters.brightness_alpha_range)
             beta = np.random.uniform(*Parameters.brightness_beta_range)
             return cv2.convertScaleAbs(res_image, alpha=alpha, beta=beta)
 
-        elif method == 'blur':
-            kernel_size = np.random.randint(*Parameters.blur_kernel_range)
-            return cv2.blur(res_image, (kernel_size, kernel_size))
-
-        else:
-            raise NotImplementedError('Method {} is not implemented'.format(method))
+        return res_image
 
     @staticmethod
     def generate_images(images, image_count):
@@ -269,22 +253,123 @@ class Model:
     def __init__(self):
         self.__classifier = None
 
-    def fit(self, train_data, train_labels, validation_data, validation_labels):
+    def fit(self, train_data, train_labels):
         self.__classifier = RandomForestClassifier(
             max_features="auto",
             n_estimators=Parameters.n_trees,
             n_jobs=-1,
-            criterion="gini",
+            criterion=Parameters.criterion,
             min_samples_leaf=Parameters.min_samples_leaf,
             max_depth=Parameters.max_depth
         )
         self.__classifier.fit(train_data, train_labels)
-        validation = self.__classifier.predict(validation_data)
-        return self.accuracy_score(validation_labels, validation)
+        train_pred = self.__classifier.predict(train_data)
+        return accuracy_score(train_labels, train_pred)
 
-    def predict(self, test_data):
-        return self.__classifier.predict(test_data)
+    def predict(self, data):
+        return self.__classifier.predict(data)
+
+
+class Test:
 
     @staticmethod
-    def accuracy_score(test_labels, predicted):
-        return accuracy_score(test_labels, predicted)
+    def build_freq_chart(training_labels):
+        labels = list(range(Parameters.class_count))
+        freqs = [training_labels.count(label) for label in labels]
+        plot = plt.bar(labels, freqs)
+        plt.ylabel("Frequencies")
+        plt.xlabel("Class")
+        plt.title("Number of examples per each class")
+
+        return plot
+
+    @staticmethod
+    def timed_function(f, *args, **kwargs):
+        start = time.time()
+        res = f(*args, **kwargs)
+        end = time.time()
+        return res, end - start
+
+    @staticmethod
+    def load_data(data: DataUtils):
+        data, curr_time = Test.timed_function(data.load_data)
+        print("Load time {:0.2f}s".format(curr_time))
+
+    @staticmethod
+    def preprocess_data(data: DataUtils):
+        _, curr_time = Test.timed_function(data.transform_data)
+        print("Transform: {:0.2f}s".format(curr_time))
+
+        _, curr_time = Test.timed_function(data.train_validation_split)
+        print("Split: {:0.2f}s".format(curr_time))
+
+        if Parameters.enable_augment:
+            _, curr_time = Test.timed_function(data.augment)
+            print("Augment: {:0.2f}s".format(curr_time))
+        else:
+            print("Augment disabled by config")
+
+        _, curr_time = Test.timed_function(data.normalize_images)
+        print("Normalization: {:0.2f}s".format(curr_time))
+
+    @staticmethod
+    def train_model(data, model):
+        score, curr_time = Test.timed_function(model.fit, data.training_data, data.training_labels)
+        print("Train set accuracy: {}. \nModel fit: {:0.2f}s".format(score, curr_time))
+
+        return model
+
+    @staticmethod
+    def get_predicted(dataset, model):
+        predicted, curr_time = Test.timed_function(model.predict, dataset)
+        print("Prediction: {:0.2f}s".format(curr_time))
+        return predicted
+
+    @staticmethod
+    def print_scores(real, predicted, label: str):
+        accuracy = accuracy_score(real, predicted)
+        precision = precision_score(real, predicted, average='macro')
+        recall = recall_score(real, predicted, average='macro')
+        print("{} set accuracy: {:0.2f}%, precision: {:0.2f}%, recall: {:0.2f}%".
+              format(label, accuracy * 100, precision * 100, recall * 100))
+
+    @staticmethod
+    def show_confusion_matrix(real, predicted, label: str):
+        matrix = confusion_matrix(real, predicted, labels=list(range(Parameters.class_count)))
+        matrix = matrix.astype('float') / matrix.sum(axis=1)[:, np.newaxis]
+
+        df = pd.DataFrame(matrix)
+        plot = sn.heatmap(df, xticklabels=3, yticklabels=3)  # font size
+        y_lim = list(plot.get_ylim())
+        y_lim[0] += 0.5
+        y_lim[1] = 0
+        plot.set_ylim(y_lim)
+        plt.title("Confusion matrix for {} set".format(label))
+        plt.show()
+
+        return matrix
+
+    @staticmethod
+    def main():
+        data = DataUtils()
+        model = Model()
+
+        start = time.time()
+
+        Test.load_data(data)
+        Test.preprocess_data(data)
+        Test.train_model(data, model)
+
+        val_predicted = Test.get_predicted(data.validation_data, model)
+        test_predicted = Test.get_predicted(data.testing_data, model)
+        print("validation:", val_predicted)
+        print("test:", test_predicted)
+        Test.print_scores(data.validation_labels, val_predicted, 'validation')
+        Test.print_scores(data.testing_labels, test_predicted, 'test')
+
+        end = time.time()
+        print("Total time: {:0.2f}s".format(end - start))
+
+
+if __name__ == '__main__':
+    Test.main()
